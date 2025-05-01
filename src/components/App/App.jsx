c31243341b1b82bc06a46042da406f12d742417d
hanna-uler <hannauler@gmail.com>
2025-05-01 17:39:32 -0400

    add filter func but need to fix it

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,17 +5,35 @@ import SearchBox from '../SearchBox/SearchBox'
 
 export default function App() {
   const [contacts, setContacts] = useState([
-  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-]
-)
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ])
+  
+  const filterContacts = query => {
+  setContacts(contacts.filter(contact => contact.name.toLowerCase().includes(query.toLowerCase())))
+  } 
+  
+  // const [filterWords, setFilter] = useState('');
+//   const addContact = (newContact) => {
+//     setContacts((contacts) => {
+// return [...contacts, newContact]})
+  //   }
+  
+  // const handleSearchQuery = query => {
+  //   setFilter(query);
+  // } 
+
+//   const handleFilter = filterWords => {
+//   setContacts(contacts.filter(contact => contact.name.toLowerCase().includes(filterWords.toLowerCase())))
+// }
+
   return (
     <div>
       <h1>Phonebook</h1>
       {/* <ContactForm /> */}
-      <SearchBox />
+      <SearchBox onChange={filterContacts} />
       <ContactList contacts={contacts} />
     </div>
 
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,15 +1,17 @@
 import css from './SearchBox.module.css'
 import { useState } from 'react'
 
-export default function SearchBox() {
-    const [inputValue, setInputValue] = useState('');
-    const handleChange = (evt) => {
+export default function SearchBox({onChange}) {
+    const [inputValue, setInputValue] = useState('')
+    const handleSearchChange = evt => {
         setInputValue(evt.target.value);
+        onChange(inputValue);
     }
     return (
         <div>
             <label htmlFor='searchInput'>Find contacts by name</label>
-            <input type='text' id='searchInput' onChange={handleChange} value={inputValue} />
+            <input type='text' id='searchInput' onChange={handleSearchChange} value={inputValue} />
+            <p>{inputValue}</p>
         </div>
     )
     
