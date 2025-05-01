eccfb490d239a6eacf1974468d43ad2935a5d2e3
hanna-uler <hannauler@gmail.com>
2025-05-01 13:58:49 -0400

    add modern-normilize, create structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import "modern-normalize"
+import 'modern-normalize'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './components/App/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
