9e70b97d659143c4c3a4266e4dd4b50b2b36a17c
hanna-uler <hannauler@gmail.com>
2025-05-05 15:57:58 -0400

    install Inter font, add font

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App/App.jsx'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/700.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
